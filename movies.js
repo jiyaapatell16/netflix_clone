ac3df68b94170b2307e9c03db02e519265ffdab6
Jiya Patel <jiya.ptel2007@gmail.com>
2026-08-25 07:45:30 +0530

    Add movie section data model

---
+++ movies.js
@@ -0,0 +1,7 @@
+const movieSections = {
+    newReleases: ['Stranger Things', 'Wednesday', 'The Witcher'],
+    trending: ['Money Heist', 'Dark', 'Bridgerton'],
+    popular: ['Black Mirror', 'You', 'The Crown']
+};
+
+export default movieSections;
